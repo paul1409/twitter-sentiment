9c090551638d4dba5de77de58a34b8019eeba8aa
austinwilson1224 <austin1224@gmail.com>
2021-11-29 14:08:50 -0800

    deployable neural network models with weights

--- data/deployable_model.py
+++ data/deployable_model.py
@@ -28,20 +28,30 @@ model1=Sequential()
 model1.add(Dense(25, input_dim=input_dim,activation='relu'))
 model1.add(Dense(2, activation='softmax'))
 model1.compile(loss='categorical_crossentropy',optimizer='adam')
-
 model1.load_weights("data/model1.hdf5")
 
-test_pred = ['I really love sentiment analysis. It is the best and I am so happy']
+model2=Sequential()
+model2.add(Dense(25, input_dim=input_dim,activation='relu'))
+model2.add(Dense(50, input_dim=input_dim,activation='relu'))
+model2.add(Dense(25, input_dim=input_dim,activation='relu'))
+model2.add(Dense(10, input_dim=input_dim,activation='relu'))
+model2.add(Dense(2, activation='softmax'))
+model2.compile(loss='categorical_crossentropy',optimizer='adam')
+model2.load_weights("data/model2.hdf5")
 
 
-test_pred_array = vectorizer.transform(test_pred)
-test_pred_array = test_pred_array.toarray()
+
+# test_pred = ['I really love sentiment analysis. It is the best and I am so happy']
+
+
+# test_pred_array = vectorizer.transform(test_pred).toarray()
+# test_pred_array = test_pred_array.toarray()
 
 
 
-prediction = model1.predict(test_pred_array)
-prediction = np.argmax(prediction)
-labels[prediction]
+# prediction = model1.predict(test_pred_array)
+# prediction = np.argmax(prediction)
+# labels[prediction]
 
 
 def get_predictions(text, model):
@@ -52,6 +62,10 @@ def get_predictions(text, model):
         'positive': prediction[1]
     }
 
-test = get_predictions('I really love sentiment analysis. It is the best and I am so happy', model1)
+text = 'I really love sentiment analysis. It is the best and I am so happy'
+pred_model1 = get_predictions(text, model1)
+pred_model2 = get_predictions(text, model2)
+pred_model1
+pred_model2
 
-test
+
